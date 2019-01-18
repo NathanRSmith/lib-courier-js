30801c59a9566f02ea8487daa64b73f2dcaec093
Nathan Smith <nathanrandal@gmail.com>
2019-01-17 19:23:01 -0500

    Removed "parent" concept and added courier network concept. The network makes it more efficient to traverse namespaces and find an appropriate handler. For the service framework, added module groups for grouping modules under namespaces. Added canned-replier module. Updated tests

--- test/namespace.js
+++ test/namespace.js
@@ -19,6 +19,9 @@ module.exports = {
     return c1.request({}, 'test', 'hi')
       .then(() => { throw new Error('should not reach here'); })
       .catch(err => assert.equal(err.message, 'No handler registered for request "test"'))
+      .then(() => c1.request({}, 'BLAH:fail', 'hi'))
+      .then(() => { throw new Error('should not reach here'); })
+      .catch(err => assert.equal(err.message, 'No handler registered for request "BLAH:fail"'))
       .then(() => c1.request({}, 'BLAH:test', 'hi'))
       .then(res => assert.equal(res, 'HI'));
   },
@@ -46,6 +49,56 @@ module.exports = {
       assert.equal(err.message, 'Namespace already registered for "BLAH:"');
       return true;
     });
+  },
+
+  'should override namespace': function() {
+    var c1 = new Courier();
+    var c2 = new Courier();
+    c2.reply('test', function(ctx, data) {
+      assert.equal(data, 'hi');
+      return data.toUpperCase();
+    });
+
+    c1.registerNamespace('BLAH:', c2);
+    c1.registerNamespace('BLAH:', c2, {override: true});
+
+    return c1.request({}, 'test', 'hi')
+      .then(() => { throw new Error('should not reach here'); })
+      .catch(err => assert.equal(err.message, 'No handler registered for request "test"'))
+      .then(() => c1.request({}, 'BLAH:test', 'hi'))
+      .then(res => assert.equal(res, 'HI'));
+  },
+
+  'should retain the prefix if specified': function() {
+    var c1 = new Courier();
+    var c2 = new Courier();
+    c2.reply('BLAH:test', function(ctx, data) {
+      assert.equal(data, 'hi');
+      return data.toUpperCase();
+    });
+
+    c1.registerNamespace('BLAH:', c2, {retainPrefix: true});
+
+    return c1.request({}, 'test', 'hi')
+      .then(() => { throw new Error('should not reach here'); })
+      .catch(err => assert.equal(err.message, 'No handler registered for request "test"'))
+      .then(() => c1.request({}, 'BLAH:test', 'hi'))
+      .then(res => assert.equal(res, 'HI'));
+  },
+
+  'should prevent cycles': function() {
+    var child = new Courier();
+    var parent = new Courier();
+
+    parent.reply('test', function(ctx, data) {
+      assert.equal(data, 'hi');
+      return data.toUpperCase();
+    });
+
+    parent.registerNamespace('BLAH:', child, {retainPrefix: true});
+    return child.request({}, 'BLAH:test', 'hi')
+      .then(() => { throw new Error('should not reach here'); })
+      .catch(err => assert.equal(err.message, 'No handler registered for request "BLAH:test"'));
   },
 
 }
